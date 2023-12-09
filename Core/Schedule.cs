838bb1611f76570e2f24db9b3494a9fcb4d2f83d
guven.asci@emakin.com <guven.asci@emakin.com>
2023-12-09 11:06:51 +0300

    Use customizable business hours for each day of week

--- Core/Schedule.cs
+++ Core/Schedule.cs
@@ -2,40 +2,13 @@ namespace Core;
 
 public class Schedule
 {
-    public List<DayOfWeek> BusinessDays { get; set; } = new List<DayOfWeek>
-    {
-        DayOfWeek.Monday,
-        DayOfWeek.Tuesday,
-        DayOfWeek.Wednesday,
-        DayOfWeek.Thursday,
-        DayOfWeek.Friday
-    };
-
-    public List<TimeRange> BusinessHours { get; set; } = new List<TimeRange>()
-    {
-        new TimeRange()
-        {
-            Start = new TimeSpan(9, 0, 0),
-            End = new TimeSpan(12, 0,0)
-        },
-        new TimeRange()
-        {
-            Start = new TimeSpan(13, 0, 0),
-            End = new TimeSpan(18, 0, 0)
-        }
-    };
-
+    public Dictionary<DayOfWeek, List<TimeRange>> BusinessDays { get; set; } = new Dictionary<DayOfWeek, List<TimeRange>>();
     public List<DateRange> AnnualHolidays { get; set; } = new List<DateRange>();
-
     public List<DateRange> OneTimeHolidays { get; set; } = new List<DateRange>();
-    
+
+
     public bool IsBusinessDay(DateTime date)
     {
-        if (!this.BusinessDays.Contains(date.DayOfWeek))
-        {
-            return false;
-        }
-
         if (this.AnnualHolidays.Any(h => h.Start <= date && date <= h.End))
         {
             return false;
@@ -45,8 +18,13 @@ public class Schedule
         {
             return false;
         }
-
-        if(!this.BusinessHours.Any(h => h.Start <= date.TimeOfDay && date.TimeOfDay <= h.End))
+        
+        if (!this.BusinessDays.Keys.Contains(date.DayOfWeek))
+        {
+            return false;
+        }
+        
+        if (!this.BusinessDays[date.DayOfWeek].Any(h => h.Start <= date.TimeOfDay && date.TimeOfDay <= h.End))
         {
             return false;
         }
--- UnitTests/CalendarTests.cs
+++ UnitTests/CalendarTests.cs
@@ -6,10 +6,89 @@ namespace UnitTests;
 public class Tests
 {
     private static readonly Schedule schedule = new Schedule();
-    
+
     [SetUp]
     public void Setup()
     {
+        schedule.BusinessDays = new Dictionary<DayOfWeek, List<TimeRange>>()
+        {
+            {
+                DayOfWeek.Monday, new List<TimeRange>()
+                {
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(9, 0, 0),
+                        End = new TimeSpan(12, 0, 0)
+                    },
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(13, 0, 0),
+                        End = new TimeSpan(18, 0, 0)
+                    }
+                }
+            },
+            {
+                DayOfWeek.Tuesday, new List<TimeRange>()
+                {
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(9, 0, 0),
+                        End = new TimeSpan(12, 0, 0)
+                    },
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(13, 0, 0),
+                        End = new TimeSpan(18, 0, 0)
+                    }
+                }
+            },
+            {
+                DayOfWeek.Wednesday, new List<TimeRange>()
+                {
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(9, 0, 0),
+                        End = new TimeSpan(12, 0, 0)
+                    },
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(13, 0, 0),
+                        End = new TimeSpan(18, 0, 0)
+                    }
+                }
+            },
+            {
+                DayOfWeek.Thursday, new List<TimeRange>()
+                {
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(9, 0, 0),
+                        End = new TimeSpan(12, 0, 0)
+                    },
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(13, 0, 0),
+                        End = new TimeSpan(18, 0, 0)
+                    }
+                }
+            },
+            {
+                DayOfWeek.Friday, new List<TimeRange>()
+                {
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(9, 0, 0),
+                        End = new TimeSpan(12, 0, 0)
+                    },
+                    new TimeRange()
+                    {
+                        Start = new TimeSpan(13, 0, 0),
+                        End = new TimeSpan(18, 0, 0)
+                    }
+                }
+            }
+        };
+
         var christmas = new DateRange()
         {
             Start = new DateTime(2024, 1, 1, 0, 0, 0),
@@ -17,19 +96,22 @@ public class Tests
         };
 
         schedule.AnnualHolidays.Add(christmas);
-        
+
         var happyFriday = new DateRange()
         {
             Start = new DateTime(2024, 1, 5, 13, 0, 0),
             End = new DateTime(2024, 1, 5, 18, 0, 0)
         };
-        
+
         schedule.OneTimeHolidays.Add(happyFriday);
     }
 
     [TestCase("2024-01-01T09:00:00", ExpectedResult = false)]
     [TestCase("2024-01-02T09:00:00", ExpectedResult = true)]
+    [TestCase("2024-01-02T12:00:00", ExpectedResult = true)]
+    [TestCase("2024-01-02T12:30:00", ExpectedResult = false)]
     [TestCase("2024-01-05T14:00:00", ExpectedResult = false)]
+    [TestCase("2024-01-06T14:00:00", ExpectedResult = false)]
     public bool IsBusinessDay(string dateStr)
     {
         var date = DateTime.Parse(dateStr);
